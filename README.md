b4b9531b392a73a719e8c8dbcf23e15f0fb28379
Sergii Kliuchnyk <sergiikliuchnyk@gmail.com>
2012-12-24 18:33:30 +0200

    readme

--- README.md
+++ README.md
@@ -1,4 +1,36 @@
-jquery.addformdata
+jQuery addFormData plugin
 ==================
 
-jquery.addFormData plugin
+#Usage
+
+We have some form
+```html
+<form></form>
+```
+some extra data
+```javascript
+var data = {
+    obj: {
+        field: 'val'
+    },
+    arr: [1,2],
+    field: 'val'
+};
+```
+
+And here how we can add this data to our form before submit
+```javascript
+$('form').submit(function(){
+    $(this).addFormData(data);
+});
+```
+
+Result
+```html
+<form>
+    <input type="hidden" name="obj[field]" value="val"/>
+    <input type="hidden" name="arr[0]" value="1"/>
+    <input type="hidden" name="arr[1]" value="2"/>
+    <input type="hidden" name="field" value="val"/>
+</form>
+```
---
+++ jquery.addformdata.js
@@ -0,0 +1,26 @@
+(function($){
+    var unserialise = function(obj, prefix) {
+        var arr = [];
+        for(var field in obj) {
+            var name  = prefix ? prefix + '[' + field + ']' : field,
+                value = obj[field];
+            if( typeof value === 'object' ) {
+                arr = arr.concat(unserialise(value, name));
+            }
+            else {
+                arr.push({name: name, value: value});
+            }
+        }
+
+        return arr;
+    };
+
+    $.fn.addFormData = function(name, obj){
+        var arr = unserialise(obj, name);
+        for(var i = 0, len = arr.length; i < len; i++) {
+            this.append(
+                $('<input type="hidden">').attr(arr[i])
+            );
+        }
+    };
+})(jQuery);
